e140c84f64f6879f32ef0b930e1fc1ba4e184831
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-10-01 03:47:08 -0600

    FrontEnd
    Terminado el front-end, falta conexión con DB

--- Portal-Usuario/inicio.php
+++ Portal-Usuario/inicio.php
@@ -41,28 +41,33 @@
     
     <div class="d-flex">
         <!-- SIDEBAR -->
-        <div id="sidebar-container" class="bg-light shadow">
+        <div id="sidebar-container" class="bg-light shadow border border-right">
             <div class="logo mx-auto">
                 <img src="./img/amex-logo-white.png" alt="logo" class="mx-auto d-block">
             </div>
             <div class="menu" >
-                <a href="#" class="d-block px-3 py-1">Home</a>
+                <div class="d-block px-3 py-1">
+                    <a href="#" class="btn bg-primary" style="width:80%">Home</a>
+                </div>
                 <a href="#" class="d-block px-3 py-1">
                     <img src="./img/tarjeta/The_Gold_Elite_Credit_Card_American_Express.png" alt="">
                 </a>
             </div>
         </div>
-        <!--ENCABEZADO -->
-        <div class="w-100">
-            <nav class="navbar navbar-expand-lg bg-light">
+        <!--CONTENIDO -->
+        <div class="w-100 bg-gray">
+            <!--ENCABEZADO -->
+            <nav class="navbar navbar-expand-lg bg-light border-bottom" id="encabezado_info">
                 <div class="container-fluid">
-                    <div class="container text-center">
+                    <div class="container text-center" id="nombre_tarjeta">
                         <div id="nombre_cabecera">
                             <?php 
                                 echo $nombre;
                             ?>
                         </div>
-                        <div id="tarjeta">12345678901234</div>
+                        <div id="tarjeta">
+                            12345678901234
+                        </div>
                     </div>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -75,7 +80,7 @@
                                     <li><a class="dropdown-item" href="#">Action</a></li>
                                     <li><a class="dropdown-item" href="#">Another action</a></li>
                                     <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="./logout.php">Cerrar sesion</a></li>
+                                    <li style="background-color: red;"><a class="dropdown-item" href="./logout.php">Cerrar sesion</a></li>
                                 </ul>
                             </div>
                             </li>
@@ -84,6 +89,104 @@
                     </div>
                 </div>
             </nav>
+            <!-- Informacón -->
+            <div id="Información">
+                <!-- VALORES -->
+                <section class="pt-3 bg-mix">
+                    <div class="container">
+                        <div class="card">
+                            <div class="card-body p-1">
+                                <div class="row">
+                                    <div class="col-lg-4 stat my-3 d-flex">
+                                        <div class="mx-auto">
+                                            <h6>Monto autorizado</h6>
+                                            <h3 class="text-success">$50,000</h3>
+                                            <h6>50.50%</h6>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4 stat my-1 d-flex">
+                                        <div class="mx-auto">
+                                            <h5>Monto disponible</h5>
+                                            <h2>$50,000</h2>
+                                            <h5>50.50%</h5>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4 my-3 d-flex">
+                                        <div class="mx-auto">
+                                            <h6>Saldo a pagar</h6>
+                                            <h3 class="text-danger">$50,000</h3>
+                                            <h6>50.50%</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- TABLA -->
+                <section class="bg-gray">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 my-3">
+                                <div class="card rounded-1">
+                                    <div class="card-header bg-light">
+                                        <h6 class="font-weight-bold mb-0">Número de usuarios de paga</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <table class="table table-striped table-hover">
+                                            <thead>
+                                                <tr>
+
+                                                <th scope="col">#</th>
+                                                <th scope="col">Fecha</th>
+                                                <th scope="col">Descripción</th>
+                                                <th scope="col">Consumo</th>
+                                                <th scope="col">Abono</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody class="table-group-divider">
+                                                <tr>
+                                                <th scope="row">1</th>
+                                                <td>01/10/2022</td>
+                                                <td>CEMACO</td>
+                                                <td>200.00</td>
+                                                <td></td>
+                                                </tr>
+                                                <tr>
+                                                <th scope="row">2</th>
+                                                <td>01/10/2022</td>
+                                                <td>SUPER DEL BARRIO</td>
+                                                <td>150.00</td>
+                                                <td></td>
+                                                </tr>
+                                                <tr>
+                                                <th scope="row">3</th>
+                                                <td>01/10/2022</td>
+                                                <td colspan="1" class="">PAGO DE TAREJETA</td>
+                                                <td></td>
+                                                <td>300.00</td>
+                                                </tr>
+                                            </tbody>
+                                            <tfoot class="table-dark">
+                                                <tr>
+                                                <th scope="row"></th>
+                                                <td>01/10/2022</td>
+                                                <td colspan="1" class="">TOTAL</td>
+                                                <td>350.00</td>
+                                                <td>300.00</td>
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+
+
         </div>
     </div>
     
